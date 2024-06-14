792b10a874677d18708d15373b6b584bb097f9bc
Xabendlini <151826333+Xabendlini@users.noreply.github.com>
2024-06-14 11:09:49 +0200

    UI update for PatientHomeView

--- Pure Harmony App/Views/Template/PatientHomeView.xaml.cs
+++ Pure Harmony App/Views/Template/PatientHomeView.xaml.cs
@@ -102,5 +102,33 @@ namespace Pure_Harmony_App.Views
         {
             Navigation.PushAsync(new AboutUs());
         }
+
+        private void OnProfileButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+
+        async void OnVistWebsiteClicked(object sender, EventArgs e)
+        {
+            string websiteURI = "https://pubmed.ncbi.nlm.nih.gov/";
+
+
+            await Launcher.OpenAsync(websiteURI);
+        }
+
+        async void OnVisttheWebsiteClicked(object sender, EventArgs e)
+        {
+            string websiteURI = "https://www.cdc.gov/tb/default.htm";
+
+
+            await Launcher.OpenAsync(websiteURI);
+        }
+
+        async void OnVistaWebsiteClicked(object sender, EventArgs e)
+        {
+            string websiteURI = "https://www.who.int/health-topics/tuberculosis#tab=tab_1";
+
+            await Launcher.OpenAsync(websiteURI);
+        }
     }
 }
